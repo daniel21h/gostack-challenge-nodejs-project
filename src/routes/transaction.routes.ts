5edacc3bc38eaf730c6febe68f0182a606ec315a
daniel21h <hesseldaniel21@gmail.com>
2020-05-03 10:57:51 -0300

    finished challenge

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 const transactionsRepository = new TransactionsRepository();
@@ -11,8 +11,12 @@ const transactionsRepository = new TransactionsRepository();
 transactionRouter.get('/', (request, response) => {
   try {
     const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
 
-    return response.json(transactions);
+    return response.json({
+      transactions,
+      balance,
+    });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -22,9 +26,11 @@ transactionRouter.post('/', (request, response) => {
   try {
     const { title, value, type } = request.body;
 
-    const creatingDatas = { title, value, type };
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
 
-    const transaction = transactionsRepository.create(creatingDatas);
+    const transaction = createTransaction.execute({ title, value, type });
 
     return response.json(transaction);
   } catch (err) {
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,26 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    // Caso o tipo de transação inserido seja inválido(diferencial do desafio)
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('Transaction type is invalid');
+    }
+
+    // Validar se o valor da transaction que estou pegando, e se o tipo for outcome e comparar o valor com o total
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new Error('You do not have enough balance');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
